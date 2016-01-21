445c278ec676ca31bb25602f4b70eb5092b0acda
piotrek1234 <pqcinski+gh@gmail.com>
2016-01-21 17:23:56 +0100

    znajdowanie użytkowników oceniających te same filmy

--- wszystko.R
+++ wszystko.R
@@ -36,3 +36,27 @@ getRatedMovies <- function(data, userId) {
   }
   return (moviesList)
 }
+
+# zwraca liczbę wspólnie ocenionych filmów
+getCommonCount <- function(data, user1, user2) {
+  return(length(intersect(getRatedMovies(data, user1), getRatedMovies(data, user2))))
+}
+
+# zwraca wspólnie ocenione filmy
+getCommonMovies <- function(data, user1, user2) {
+  return(intersect(getRatedMovies(data, user1), getRatedMovies(data, user2)))
+}
+
+# znajduje użytkowników którzy ocenili te same filmy co user, sortuje i zwraca count wyników
+findSameMoviesWatchers <- function(data, user, count) {
+  usersCount = 943
+  users = vector()
+  for(user2 in 1:usersCount) {
+    if(user2 != user) {
+      users = append(users, c(user2, getCommonCount(data, user, user2)))
+    }
+  }
+  retval = matrix(users, ncol=2, byrow=TRUE)
+  colnames(retval) = c("userid", "sameMovies")
+  return(head(retval[order(retval[,2], decreasing = TRUE),], n=count))
+}
