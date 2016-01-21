63b7b97551814b3c8b23aabbd6ee7d6b5b4fd00e
piotrek1234 <pqcinski+gh@gmail.com>
2016-01-21 21:42:00 +0100

    szkielet porównywania użytkowników

--- wszystko.R
+++ wszystko.R
@@ -60,3 +60,35 @@ findSameMoviesWatchers <- function(data, user, count) {
   colnames(retval) = c("userid", "sameMovies")
   return(head(retval[order(retval[,2], decreasing = TRUE),], n=count))
 }
+
+# modyfikuje dane zastępująć oceny liczbą 1 lub 0 (w zależności czy ocena różni się
+# nie więcej niż diff od oceny użytkownika)
+modifyDataSameRating <- function(data, users, user, diff) {
+  # users można zastąpić przez findSameMoviesWatchers(data, user, ileś)[,1]
+  moviesCount = 1682
+  usersCount = 943
+  ratings = matrix(rep(NA, (moviesCount+1)*length(users)), byrow=TRUE, ncols=moviesCount)
+  # w ratings będą siedzieć informacje czy ocena jest bliska czy nie
+  i=1
+  for(u in users) {
+    for(m in 1:moviesCount) {
+      # ostatnia kolumna - id użytkownika
+      ratings[i, moviesCount+1] = u
+      # sprawdzić czy jeden ma wpisane 0 (nie oglądał) i coś z tym zrobić
+      # jeśli różnica jest mniejsza niż diff to gdzieś wstawić
+      if(abs(data[u, m] - data[user, m]) < diff) {
+        # różnica ocen mniejsza niż diff
+        # rozkminić indeksy czy są na pewno spoko
+        ratings[i, m] = 1
+      }
+      else {
+        ratings[i, m] = 0
+      }
+      i=i+1
+    }
+    # powycinać kolumny ze zbędnymi filmami
+    # wynikiem ma być macierz: wiersz to użytkownik, w kolumnach są filmy, a wartość 1 lub 0
+    # to info czy ocenił film tak jak my. W pierwszej kolumnie można ewentualnie powpisywać
+    # id użytkownika
+  }
+}
